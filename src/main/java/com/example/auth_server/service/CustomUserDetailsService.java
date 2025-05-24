284cec20ae88e99e8445081a3ec1e9f8e95ccd61
friara <afririan@outlook.com>
2025-05-24 13:02:29 +0300

    Update CustomUserDetailsService.java

--- src/main/java/com/example/auth_server/service/CustomUserDetailsService.java
+++ src/main/java/com/example/auth_server/service/CustomUserDetailsService.java
@@ -8,6 +8,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -22,6 +23,7 @@ public class CustomUserDetailsService implements UserDetailsService {
         this.userRepository = userRepository;
     }
 
+    @Transactional(readOnly = true)
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         User user = userRepository.findByLogin(username)
